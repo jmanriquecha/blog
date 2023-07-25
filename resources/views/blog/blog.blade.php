c63d9ed06531d82d023a7d945c5476087dfdc591
Jorge Manrique <georgemanc.28@gmail.com>
2023-07-25 14:33:10 -0500

    Se mejoran las vistas y se establece relación entre users y posts

--- resources/views/blog/blog.blade.php
+++ resources/views/blog/blog.blade.php
@@ -2,9 +2,13 @@
     @section('content')
         <h1>Posts</h1>
         @foreach ($posts as $post)
-            <div class="card mb-2">
-                <div class="card-header">
+            <div class="card mb-3">
+                <div class="card-body pt-3 pb-4">
                     <a href="{{ url('blog/' . $post->slug) }}" class="nav-link">{{ $post->title }}</a>
+                </div>
+                <div @class(['border-top', 'card-footer' => true])>
+                    <span @class(['fw-bold', 'small' => true])>Creado por:</span>
+                    <span @class(['small'])>{{ $post->user->name }} | {{ $post->created_at }}</span>
                 </div>
             </div>
         @endforeach
--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -10,38 +10,42 @@
 </head>
 
 <body>
-    {{-- Start nav --}}
+    @auth
+        {{-- Start nav --}}
 
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Blog</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="{{ route('home') }}">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('blog') }}">Blog</a>
-                    </li>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Blog</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="{{ route('home') }}">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('blog') }}">Blog</a>
+                        </li>
 
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
             </div>
-        </div>
-    </nav>
+        </nav>
 
-    {{-- End nav --}}
-    <div class="container">
-        @yield('content')
-    </div>
+        {{-- End nav --}}
+        <div class="container">
+            @yield('content')
+        </div>
+    @else
+        <a href="{{ route('login') }}" @class(['btn btn-primary'])>Login</a>
+    @endauth
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
