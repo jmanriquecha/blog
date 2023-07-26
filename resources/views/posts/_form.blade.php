74ceb3437e676d4d8f49c5cc3ec5809a5c8d5e7d
Jorge Manrique <georgemanc.28@gmail.com>
2023-07-26 10:46:55 -0500

    Se crean validaciones para los campos requeridos

--- resources/views/posts/_form.blade.php
+++ resources/views/posts/_form.blade.php
@@ -1,10 +1,21 @@
 @csrf
 
 <label for="title" class="uppercase text-gray-700 text-xs">Título</label>
-<input type="text" name="title" id="title" class="rouded border-gray-200 w-full mb-4" value="{{ $post->title }}">
+<span class="text-xs text-red-600">
+    @error('title')
+        {{ $message }}
+    @enderror
+</span>
+<input type="text" name="title" id="title" class="rouded border-gray-200 w-full mb-4"
+    value="{{ old('title', $post->title) }}">
 <label for="body" class="uppercase text-gray-700 text-xs">Contenido</label>
+<span class="text-xs text-red-600">
+    @error('body')
+        {{ $message }}
+    @enderror
+</span>
 <textarea name="body" id="body" rows="10" class="rouded border-gray-200 w-full mb-4">
-    {{ $post->body }}
+    {{ old('body', $post->body) }}
 </textarea>
 
 <div class="flex items-center justify-between">
