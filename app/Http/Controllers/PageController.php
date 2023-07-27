00f1e7f3822b1e7b314e05bb6265569988160768
Jorge Manrique <georgemanc.28@gmail.com>
2023-07-27 09:27:48 -0500

    Se mejora la visualización de los blog incluye usuario que lo realiza

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -10,7 +10,9 @@ class PageController extends Controller
     public function home(Request $request)
     {
         $search = $request->search;
-        $posts = Post::where('title', 'LIKE', "%$search%")->latest()->paginate(10);
+        $posts = Post::where('title', 'LIKE', "%$search%")
+                ->with('user')
+                ->latest()->paginate(10);
 
         return view('blog.home', ['posts' => $posts]);
     }
--- resources/views/blog/home.blade.php
+++ resources/views/blog/home.blade.php
@@ -23,6 +23,15 @@
                     </p>
 
                     <h2 class="text-lg text-gray-900 mt-2 px-2"> {{ $post->title }}</h2>
+                    <hr class="border-1 opacity-75 mt-3 w-50">
+                    <span class="flex items-center px-2 py-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-3 h-3">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                        </svg>
+                        <span class="ml-1 text-gray-600 text-xs">{{ $post->user->name }}</span>
+                    </span>
                 </a>
             @endforeach
         </div>
